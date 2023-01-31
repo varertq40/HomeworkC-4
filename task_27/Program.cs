989e5a2913b611f4fee5232e2d3e0ad92a4ac3b1
Igor <jasinski1998@gmail.com>
2023-01-31 19:53:04 +0300

    task_27

---
+++ task_27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+void Sum(string[] args)
+{
+    Console.WriteLine("Введите число");
+    int b = int.Parse(Console.ReadLine()!);
+    int summa = 0;
+    int temp = 0;
+    {
+        while (b > 0)
+        {
+            temp = b % 10;
+            summa = summa + temp;
+            b = b / 10;
+        }
+        Console.WriteLine(summa);
+    }
+}
+
+Sum(args);
